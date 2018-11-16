887445c132e4ae69ad2bb8315e5eb3cbd36ec4eb
tomystery <931892155@qq.com>
2018-11-16 11:11:55 +0800

    webpack4的配置全部完成

--- README.md
+++ README.md
@@ -1,40 +1,13 @@
+# 1. 两中环境下的不同
 
-##项目初始化步骤
+1. dev环境下要开启sourceMap 
+2. 开发环境style-loader,生产环境extra-text-webpack-plugin,处理css文件
+3. 开发环境file-loader,生产环境url-loader处理图片、字体图标那些
+4. dev环境下没有clean 目录
+5. dev下有dev-server
 
-* 安装nodejs环境,推荐使用v6.12.3
-    下载地址 : https://nodejs.org/download/release/v6.12.3/
+# 2. webpack4相对于之前版本的改版
 
-* 在项目根目录执行npm初始化
-    命令: npm install --registry=https://registry.npm.taobao.org
-
-* 启动项目
-    开发模式: npm run dev (windows系统上为npm run dev_win)
-    生产模式: npm run dist (windows系统上为npm run dist_win)
-
-* 开发模式下预览项目
-    访问：http://localhost:8088/dist/view/index.html
-
-
----
-##mmall-fe v1.0.1改动：
-
-* 升级nodejs版本到6.12.3（升级后需要把原项目中node_modules目录删除，重新执行初始化）
-
-* hogan插件被废弃，替换为hogan.js
-
-* 启动方式改动，不再需要安装全局的webpack和webpack-dev-server（需要nodejs版本在6.12.3）
-
-* 移除webpack-dev-server里的client注入，webpack-dev-server可以自动注入client了
-
-* 添加devServer的proxy选项，开发模式下可以直接访问接口，不再需要charles或fiddler做代理
-
-* 将启动命令中的inline和port选项从命令移到了配置文件中
-
-
----
-##mmall-fe v1.0.2改动：
-
-* 升级webpack到4.2.0版本，并升级相关插件
-
-* 将小于2kb的图片进行base64转码
-
+1. 新增mode，而这个mode可以使得分别在生产和开发环境下少去配置plugin
+2. optimization.splitChunks
+3. optimization.minimizer(uglifyjs)-指定压缩器
---
+++ webpack.dev.config.js
@@ -0,0 +1,202 @@
+let webpack = require('webpack')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+let path = require('path')
+
+// 获取html-webpack-plugin参数的方法 
+var getHtmlConfig = function (name, title) {
+    return {
+        template: './src/view/' + name + '.html',
+        filename: 'view/' + name + '.html',
+        favicon: './favicon.ico',
+        title: title,
+        inject: true,
+        hash: true,
+        chunks: ['common', name]
+    };
+};
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'common': './src/page/common/index.js',
+        'index': './src/page/index/index.js',
+        'list': './src/page/list/index.js',
+        'detail': './src/page/detail/index.js',
+        'cart': './src/page/cart/index.js',
+        'order-confirm': './src/page/order-confirm/index.js',
+        'order-list': './src/page/order-list/index.js',
+        'order-detail': './src/page/order-detail/index.js',
+        'payment': './src/page/payment/index.js',
+        'user-login': './src/page/user-login/index.js',
+        'user-register': './src/page/user-register/index.js',
+        'user-pass-reset': './src/page/user-pass-reset/index.js',
+        'user-center': './src/page/user-center/index.js',
+        'user-center-update': './src/page/user-center-update/index.js',
+        'user-pass-update': './src/page/user-pass-update/index.js',
+        'result': './src/page/result/index.js',
+        'about': './src/page/about/index.js',
+        'test': './src/page/test/index.js'
+    },
+    output: {
+        /* 
+        * 【改动】：删除path的配置，在webpack4中文件默认生成的位置就是/dist,
+        *  而publicPath和filename特性的设置要保留
+        */
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'js/[name].js'
+    },
+    module: {
+        /* 
+        * 【改动】：loader的使用中，loaders字段变为rules，用来放各种文件的加载器，用rules确实更为贴切
+        */
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            compact: false
+                        }
+                    }
+                ],
+                // 不检查node_modules下的js文件
+                exclude: "/node_modules/"
+            },
+            /* 
+            * 【改动】：css样式的加载方式变化
+            */
+            // css文件的处理
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options:{
+                                sourceMap:true
+                            }
+                        },
+                        { loader: "postcss-loader" }]
+                })
+            },
+            {
+                test: /\.less$/,
+                // 三个loader的顺序不能变
+                // 不分离的写法
+                // use: ["style-loader", "css-loader", "less-loader"]
+                // 分离的写法
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "postcss-loader" },
+                    {
+                        loader: "less-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            /* 
+            * 【改动】：模板文件的加载方式变化
+            */
+            // 模板文件的处理
+            {
+                test: /\.string$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            /* 
+            * 【改动】：图片文件的加载方式变化，并和字体文件分开处理
+            */
+            // 图片的配置
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            /* 
+                            * 【改动】：图片小于2kb的按base64打包
+                            */
+                            limit: 2048,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            /* 
+            * 【改动】：字体文件的加载方式变化
+            */
+            // 字体图标的配置
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            { test: /\.ejs$/, loader: 'ejs-loader?variable=data' }
+        ]
+    },
+    resolve: {
+        alias: {
+            node_modules: __dirname + '/node_modules',
+            util: __dirname + '/src/util',
+            page: __dirname + '/src/page',
+            service: __dirname + '/src/service',
+            image: __dirname + '/src/image'
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin("css/[name].css"),
+        // html模板的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall')),
+        new HtmlWebpackPlugin(getHtmlConfig('test', '关于MMall')),
+        new webpack.ProvidePlugin({
+            _: "underscore"
+        }),
+
+        //引入jquery
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    devServer: {
+        port: 8088,
+        inline: true,
+        proxy: {
+            '**/*.do': {
+                target: 'http://test.happymmall.com',
+                changeOrigin: true
+            }
+        }
+    }
+
+}
